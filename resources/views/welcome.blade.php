8841c5a0a4137ba83ff2d7e9e785fa3d38176360
Jesus Adame <desarrollo@lasmananitas.com.mx>
2021-01-09 13:02:24 -0600

    config

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,81 @@
     <div class="container mx-auto text-center">
         <h1 class="text-xl my-4">YenilikTec</h1>
 
+        <h3>Servicios</h3>
+
         <p>
-            Agencia impulsada por la innovación y el conocimiento. Ayudamos a impulsar negocios en la era digital.
+            Somos una agencia impulsada por la innovación y el conocimiento. Ayudamos a impulsar negocios en la era digital.
+        </p>
+        <p>
+            Nuestros servicios van orientados especialmente al desarrollo web que vallan alineados con los
+            objetivos de marketing digital del negocio, pensando así desde el inicio como serán las estructuras a
+            seguir tanto para las campañas de marketing como para el diseño y desarrollo de un sitio web que aumenten mas
+            la efectividad en cada una de las fases de conversión de un embudo de ventas para tu negocio.
+        </p>
+
+        <div class="flex flex-wrap">
+            <div class="w-1/2">
+                <h4>Marketing Digital</h4>
+                <p>        
+                    ¿Creas campañas publicitarias pero no te funcionan como desearas? Mejor pide asesoría,
+                    podemos ayudarte para hacer que tengas campañas publicitarias efectivas para tu negocio.
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Desarrollo web</h4>
+                <p>        
+                    Si tienes un proyecto pendiente de un sitio web para tu negocio, no lo pospongas mas, tenemos servicios a
+                    tu medida para el desarrollo de tu E-Commerce o Sitio Web
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Marketing Digital & Desarrollo Web</h4>
+                <p>        
+                    Si necesitas crear todo un proceso automatizado de ventas y captación de clientes, para generar mayores ingresos en tu negocio por medio de un embudo de negocio, contáctanos porque te podemos orientar.
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Servicios  Desarrollo Web</h4>
+                <p>        
+                    Ya sea que busques desarrollar un sistema web a la medida o la creación de cualquier sitio web para tu restaurante, tienda, panadería, estética, salón de eventos etc. puedes comunicarte, para que te ofrecerte distintas soluciones para tu problema. 
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Nuevas Tecnologías</h4>
+                <p>        
+                    Estamos preparados para ofrecerte lo mejor en desarrollo web ya sea en la nube o hosting, utilizando React, View, NodeJs, PHP, JavaScrip, entre otros según sea el caso y tu preferencia.
+                    Especialistas
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Analitic's</h4>
+                <p>
+                    Contamos con equipo de monitoreo constante de métricas, para mejorar tu rendimiento, aprovechando al máximo las campañas de marketing.
+                </p>
+            </div>
+            <div class="w-1/2">
+                <h4>Servicios Marketing Digital</h4>
+                <p>
+                    Tener campañas de marketing digital para tu negocio, es de extrema importancia ya sea si vas empezando o no, todo negocio debe desarrollar una estrategia, por ello puedes preguntar por un plan personalizado que ayude a que crezca mas rápido tu negocio.
+                    Inbound Marketing
+                </p>
+            </div>
+        </div>
+        
+        <h2>Beneficios</h2>
+        <p>
+            Equipo de desarrollo que constantemente esta investigando para ofrecer un servicio que este en la vanguardia con las nuevas tecnologías de desarrollo.
+            Tiempo de Entrega
+
+            Sabemos que lo que mas te interesa para tu negocio es el tiempo de entrega de tu proyecto, por lo cual optimizamos nuestros tiempos de trabajo para que tus proyectos esten finalizados en el menor tiempo posible.
+            Soporte al Cliente
+
+            Soporte técnico 24/7 para que te sientas mas seguro y puedas comunicarte en caso de cualquier contingencia.
+
+            Si necesitas generar mayores ventas en tu negocio, pide informe acerca de nuestros servicios de estrategias de Inbound Marketing.
+            Especialistas
+
+            No desperdicies dinero en tratar de crear tus campañas publicitarias, mejor comunícate con nuestros especialistas, para que te puedan asesorar.
         </p>
     
         <div class="flex" style="height: 100px">
@@ -17,4 +90,18 @@
         </div>
     </div>
 
+    <div class="contact bg-gray-600 py-4 text-white">
+        <div class="container m-auto">
+            <h3>CONTACTO</h3>
+    
+            Si tienes dudas acerca de los planes o paquetes o podemos ofrecerte, puedes contactarnos vía correo, whatsapp o directamente en nuestras redes sociales.
+    
+            Address
+            Cuernavaca Morelos
+            Phone
+            +52 777 344 7577
+            Email Address
+            contacto@yeniliktec.com
+        </div>
+    </div>
 </x-app-layout>

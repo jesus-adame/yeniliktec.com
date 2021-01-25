50f442d780c5345e1e8781144839e0a40de4c8bf
Jesus Adame <desarrollo@lasmananitas.com.mx>
2021-01-24 22:45:37 -0600

    Mesenger facebook

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,33 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased">
+        <!-- Load Facebook SDK for JavaScript -->
+        <div id="fb-root"></div>
+        <script>
+            window.fbAsyncInit = function() {
+                FB.init({
+                    xfbml   : true,
+                    version : 'v9.0'
+                });
+            };
+            (function(d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+                fjs.parentNode.insertBefore(js, fjs);
+            } (document, 'script', 'facebook-jssdk'));
+        </script>
+
+        <!-- Your Chat Plugin code -->
+        <div class="fb-customerchat"
+            attribution=setup_tool
+            page_id="114960133481077"
+            theme_color="#0A7CFF"
+            logged_in_greeting="Hola, ¿Cómo puedo ayudarte?"
+            logged_out_greeting="Hola, ¿Cómo puedo ayudarte?">
+        </div>
+        
         <x-jet-banner />
 
         <div class="min-h-screen flex flex-col bg-gray-100">

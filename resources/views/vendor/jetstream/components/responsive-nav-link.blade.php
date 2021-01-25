fa03bbb9ea19a57ced0c5bb9036e010563943267
Jesus Adame <desarrollo@lasmananitas.com.mx>
2021-01-25 00:06:51 -0600

    Correcciones

--- resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
+++ resources/views/vendor/jetstream/components/responsive-nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'block pl-3 pr-4 py-2 border-l-4 border-gray-400 text-base font-medium text-gray-900 bg-gray-50 focus:outline-none focus:text-white focus:bg-gray-100 focus:border-gray-700 transition duration-150 ease-in-out'
-            : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-white hover:text-gray-700 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-white focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out';
+        ? 'block pl-3 pr-4 py-2 border-l-4 border-gray-400 font-medium text-gray-900 bg-gray-50 focus:outline-none transition duration-150 ease-in-out'
+        : 'block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium hover:text-gray-700 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:bg-gray-500 focus:border-gray-300 transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>

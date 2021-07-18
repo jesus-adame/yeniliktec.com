fe12fd24c9392b9da4d2cc277ad496f1c64e4c69
Mario Angel <mario.garcia.sand@gmail.com>
2021-07-17 23:40:31 -0500

    feat: contact from analytics event

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,24 +11,26 @@
 @section('content')
 @include('components.breadcrumb')
 <div class="container mx-auto px-4 my-5">
-    <div class="flex">
-        <div class="w-1/2">
+    <div class="flex flex-wrap">
+        <div class="md:w-1/2">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3773.8092710947944!2d-99.24470158548607!3d18.93983456116105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cddf2b4feaa1b7%3A0xcce9bbd77d53f705!2sYeniliktec%20Agencia%20Digital!5e0!3m2!1ses-419!2smx!4v1626322467205!5m2!1ses-419!2smx" 
             width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
-
-        <div class="w-1/2 px-4">
+        <div class="md:w-1/2 px-4">
             <h2 class="text-center">Déjanos un mensaje</h2>
             <p class="text-xl text-center my-6">
-                Si tienes dudas acerca de los planes o paquetes que podemos ofrecerte, puedes contactarnos vía correo, whatsapp o directamente en nuestras redes sociales.
+                Si tienes dudas acerca de los planes o paquetes que podemos ofrecerte, puedes contactarnos vía correo,
+                whatsapp o directamente en nuestras redes sociales.
                 Te atenderemos lo antes posible.
             </p>
-            @if(Session::has('success'))
+            @if (session()->has('success'))
                 <div class="p-4 text-green-900 bg-green-200 border-l-5 border-green-700">
-                    {{ Session::get('success') }}
+                    {{ session()->get('success') }}
                 </div>
             @endif
-            <form class="mx-auto mb-4 mt-5" action="/sendmail" method="post">
+            <form class="mx-auto mb-4 mt-5" action="/sendmail"
+                method="post"
+                @onsubmit="gtag('event', 'enviar', {'event_category': 'contacto', 'event_label': 'formulario', value': '0'});">
                 @csrf <input type="hidden" name="page" value="contact">
                 <label class="hidden" for="name">Nombre</label>
                 <input class="w-full mb-4 shadow border-none bg-gray-50" type="text" name="name" placeholder="Nombre">
